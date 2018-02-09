e44ee0af82d2eb9b5479b289f6b85fa64a4591a4
weiguo.ni <neefrankie@gmail.com>
2018-02-09 15:05:44 +0800

    format console.log

--- test/pkce.js
+++ test/pkce.js
@@ -3,17 +3,18 @@ const {createHash} = require('crypto');
 
 async function codeChallenge() {
   const codeVerifier = await keyGen(40);
-  console.log(codeVerifier);
+  console.log('Code verfier: %s', codeVerifier);
 
+  // 1 base64 digit represents 6 bits of data. 256 / 6 = 42 ... 4
   const codeChallenge = createHash('sha256')
     .update(codeVerifier)
     .digest('base64');
-  console.log(codeChallenge);
-  console.log(codeChallenge.length);
+  console.log('Base64 codeChallenge: %s', codeChallenge);
+
   const hex = createHash('sha256')
     .update(codeVerifier)
     .digest('hex');
-  console.log(hex);
+  console.log('hex codeChallenge: %s', hex);
 }
 
 codeChallenge()
