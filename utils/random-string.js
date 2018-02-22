08c5de8a9af1ea22d217fd93f969ddb9c562988e
weiguo.ni <neefrankie@gmail.com>
2018-02-22 17:40:07 +0800

    rm

--- utils/random-string.js
+++
@@ -1,9 +0,0 @@
-const Chance = require('chance');
-const chance = new Chance();
-
-module.exports = function randomString(size=10) {
-  return chance.string({
-    pool: 'abcdedfhigklmnopqrstuvwxyz0123456789',
-    length: size
-  });
-};
